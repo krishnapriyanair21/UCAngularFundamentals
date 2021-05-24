011f7f72c7e7132d185e49abb994aae5081a14ef
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-24 12:43:42 -0700

    first component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
-    <div class="app">
-      Hello!
+  templateUrl: `
+    <div class = 'app'>
+      {{title}}
     </div>
   `
 })
-export class AppComponent {
-
+export class AppComponent{
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
