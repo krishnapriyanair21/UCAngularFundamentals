d0d51a4ef350313a5dbce17e28673e9b5e398286
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-26 10:02:46 -0700

    property binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,19 @@ import { Component } from "@angular/core";
   styleUrls: ['app.component.scss'],
   templateUrl: `
     <div>
-        {{title}}
-      <div>
+        <h1 [innerHTML] = "title"></h1>
+        <div>
+          <input type= "text" [value] = "name">
+          <div>{{ name }}</div>
+        </div>
+     <!--
+     <div>
         {{numberOne + numberTwo}}
       </div>
       <div>
         {{ isHappy ? ':)' : ':(' }}
       </div>
+      // -->
     </div>
   `
 })
@@ -20,6 +26,7 @@ export class AppComponent{
   numberOne: number = 1;
   numberTwo: number = 2;
   isHappy: boolean = true;
+  name: string = "Krishna";
   constructor() {
     this.title = 'Ultimate Angular';
   }
