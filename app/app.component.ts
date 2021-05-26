d69fb6dce15f62b8d4ac0ee7ef04f2c50f216f87
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-05-26 10:11:37 -0700

    event binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,19 +5,30 @@ import { Component } from "@angular/core";
   styleUrls: ['app.component.scss'],
   templateUrl: `
     <div>
-        <h1 [innerHTML] = "title"></h1>
-        <div>
-          <input type= "text" [value] = "name">
-          <div>{{ name }}</div>
-        </div>
-     <!--
-     <div>
+      <h1 [innerHTML] = "title"></h1>
+      <div class = "app">
+        <button (click)="handleClick()">
+          Change name
+        </button>
+        <input
+          type= "text"
+          [value] = "name"
+          (input) = "handleInput($event)"
+          (blur)="handleBlur($event)">
+      </div>
+      <div>{{ name }}</div>
+    <!--
+      <div>
+        <input type= "text" [value] = "name">
+        <div>{{ name }}</div>
+      </div>
+      <div>
         {{numberOne + numberTwo}}
       </div>
       <div>
         {{ isHappy ? ':)' : ':(' }}
       </div>
-      // -->
+    // -->
     </div>
   `
 })
@@ -29,5 +40,15 @@ export class AppComponent{
   name: string = "Krishna";
   constructor() {
     this.title = 'Ultimate Angular';
+  }
+  handleClick() {
+    this.name = "Nair";
+  }
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+  handleBlur(event: any) {
+    this.name = event.target.value;
+    console.log(event);
   }
 }
