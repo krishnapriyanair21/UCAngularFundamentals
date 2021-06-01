4ccac5f63fbf3e67d275d6fd71806282b2d134f9
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-01 08:20:32 -0700

    one way data flow overview

--- app/app.component.ts
+++ app/app.component.ts
@@ -64,3 +64,21 @@ export class AppComponent{
     children: [{ name: "Sammy", age: 1 }, { name: 'Sadie', age: 1 }]
   }]
 }
+
+/* Smart/ Dumb Component */
+// Smart component:
+  // communicates with services
+  // render child comps
+// Dumb component
+  // accepts data via inputs
+  // emit data changes via event outputs
+
+/* One- way dataflow overview */
+  // component tree with service -> smart component -> children
+  // change made in lowest level child - How to get change to service if needed
+    // pass data back up via event from child component
+    // pass for all "dumb" components via event
+    // rerender data for child nodes if needed
+  // Golden rule:
+    // Data flows down
+    // events emit up
