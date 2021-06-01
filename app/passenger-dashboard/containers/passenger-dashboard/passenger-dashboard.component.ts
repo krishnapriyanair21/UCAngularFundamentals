16e07ea3fdb8b9a1db5514ae8b1bc690e70436cf
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-01 09:23:30 -0700

    ngOnInit

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
 
 @Component({
@@ -26,30 +26,34 @@ import { Passenger } from "../../models/passenger.interface";
     `
 })
 
-export class PassengerDashboardComponent{
-    passengers: Passenger[]=[{
-        id: 1,
-        fullname: "Dave",
-        checkedIn: true,
-        checkInDate: 1490742000000,
-        children: [{name:"Buddy", age: 3}]
-      },{
-        id: 2,
-        fullname: "Dillon",
-        checkedIn: true,
-        checkInDate: 14884128000000,
-        children: [{name:"pickle", age: 7}]
-      },{
-        id: 3,
-        fullname: "Krishnapriya",
-        checkedIn: false,
-        checkInDate: null,
-        children:null
+export class PassengerDashboardComponent implements OnInit{
+    passengers: Passenger[];
+    constructor(){}
+    ngOnInit(){
+        this.passengers = [{
+            id: 1,
+            fullname: "Dave",
+            checkedIn: true,
+            checkInDate: 1490742000000,
+            children: [{name:"Buddy", age: 3}]
+        },{
+            id: 2,
+            fullname: "Dillon",
+            checkedIn: true,
+            checkInDate: 14884128000000,
+            children: [{name:"pickle", age: 7}]
+        },{
+            id: 3,
+            fullname: "Krishnapriya",
+            checkedIn: false,
+            checkInDate: null,
+            children:null
         }, {
-        id: 4,
-        fullname: "Sparky",
-        checkedIn: false,
-        checkInDate: null,
-        children: [{ name: "Sammy", age: 1 }, { name: 'Sadie', age: 1 }]
-      }]
+            id: 4,
+            fullname: "Sparky",
+            checkedIn: false,
+            checkInDate: null,
+            children: [{ name: "Sammy", age: 1 }, { name: 'Sadie', age: 1 }]
+        }
+        ]}
 }
