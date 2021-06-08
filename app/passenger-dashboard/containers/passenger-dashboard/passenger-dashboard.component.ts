3306b62a47b39ce0cfd43c5249c6ae497eb8a3ff
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-08 10:02:25 -0700

    immutable state changes

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -51,9 +51,17 @@ export class PassengerDashboardComponent implements OnInit{
     ]
   }
   handleEdit(event) {
-    console.log(event);
+    this.passengers = this.passengers.map((passenger: Passenger) => {
+      if (passenger.id === event.id) {
+        passenger = Object.assign({}, passenger, event);
+      }
+      return passenger;
+    })
+    console.log(this.passengers);
   }
   handleRemove(event) {
-    console.log(event);
+    this.passengers = this.passengers.filter((passenger: Passenger) => {
+      return passenger.id !== event.id;
+    })
   }
 }
