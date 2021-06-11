aaa17d4c16674b6a31c3ffb972ee10c036a49706
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-11 12:48:23 -0700

    tempDrivenForms

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -24,6 +24,35 @@ import { Passenger } from "../../models/passenger.interface";
       [ngModel]="detail?.id">
     </div>
 
+    <div>
+      <label>
+        <input
+        type="radio"
+        [value]="true"
+        name="checkedIn"
+        [ngModel]="detail?.checkedIn"
+        (ngModelChange)="toggleCheckIn($event)">
+        Yes
+      </label>
+      <label>
+        <input
+        type="radio"
+        [value]="false"
+        name="checkedIn"
+        [ngModel]="detail?.checkedIn">
+        No
+      </label>
+      
+      <div *ngIf="form.value.checkedIn">
+      Check in date:
+      <input
+      type="number"
+      name="checkInDate"
+      [ngModel]="detail?.checkInDate">
+      </div>
+    
+      </div>
+
     {{ form.value | json}}
   </form>
   `
@@ -33,4 +62,9 @@ export class PassengerFormComponent{
   @Input()
   detail: Passenger;
 
+  toggleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkInDate = Date.now();
+    }
+  }
 }
