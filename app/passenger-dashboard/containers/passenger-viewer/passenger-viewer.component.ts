9e6c63279011c22dd39f9da4ad03ffdccb2ef2dd
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-14 13:52:58 -0700

    route param and data fetching

--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { PassengerDashboardService } from "../../passenger-dashboard.service";
 import { Passenger } from "../../models/passenger.interface";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'passenger-viewer',
@@ -16,10 +18,14 @@ import { Passenger } from "../../models/passenger.interface";
 })
 export class PassengerViewerComponent implements OnInit{
   passenger: Passenger;
-  constructor(private passengerService: PassengerDashboardService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private passengerService: PassengerDashboardService
+  ) { }
   ngOnInit() {
-    this.passengerService
-      .getPassenger(1)
+    this.route.params
+      .switchMap((data: Passenger) => this.passengerService.getPassenger(data.id))
       .subscribe((data:Passenger)=> this.passenger = data)
   }
   onUpdatePassenger(event: Passenger) {
