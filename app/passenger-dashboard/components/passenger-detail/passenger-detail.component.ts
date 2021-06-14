78c9c13d96cfffe3a92c6f6ab05ac2d49c24dd3d
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-06-14 14:03:39 -0700

    imperative routing API

--- app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -27,6 +27,9 @@ import { Passenger } from "../../models/passenger.interface";
       <button (click)="onRemove()">
         Remove
       </button>
+      <button (click)="goToPassenger()">
+        View
+      </button>
     </div>
   `
 })
@@ -36,10 +39,13 @@ export class PassengerDetailComponent implements OnChanges{
   detail: Passenger;
 
   @Output()
-  edit: EventEmitter<any> = new EventEmitter();
+  edit: EventEmitter<Passenger> = new EventEmitter<Passenger>();
   
   @Output()
-  remove: EventEmitter<any> = new EventEmitter();
+  remove: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+  @Output()
+  view: EventEmitter<Passenger> = new EventEmitter<Passenger>();
 
   editing: boolean = false;
   constructor() { }
@@ -53,6 +59,9 @@ export class PassengerDetailComponent implements OnChanges{
     this.detail.fullname = value;
   }
 
+  goToPassenger() {
+    this.view.emit(this.detail);
+  }
   toggleEdit() {
     if (this.editing) {
       this.edit.emit(this.detail);
--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -9,6 +9,9 @@ import 'rxjs/add/operator/switchMap';
   styleUrls: ['passenger-viewer.component.scss'],
   template: `
   <div>
+    <button (click)="goBack()">
+      &lsaquo; Go Back
+    </button>
     <passenger-form
     [detail]="passenger"
     (update)="onUpdatePassenger($event)">
@@ -34,5 +37,8 @@ export class PassengerViewerComponent implements OnInit{
       .subscribe((data: Passenger) => {
         this.passenger = Object.assign({}, this.passenger, event);
     })
+  }
+  goBack() {
+    this.router.navigate(['/passengers']);
   }
 }
